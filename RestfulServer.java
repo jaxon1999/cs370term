1123e890f6e4842fa9016f032aed2e59b3c101ac
arifbiyik <63471043+arifbiyik@users.noreply.github.com>
2020-11-05 15:04:51 -0700

    Add files via upload

---
+++ RestfulServer.java
@@ -0,0 +1,60 @@
+import spark.Spark;
+import spark.Request;
+import spark.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class RestfulServer {
+
+private final Logger log = LoggerFactory.getLogger(RestfulServer.class);
+
+    public RestfulServer(){
+        this.configureRestfulApriServer();
+    }
+
+    private void configureRestfulApriServer(){
+        Spark.port(5505);
+        System.out.println("Server configured to listen on port 5505");
+    }
+    private void processRestfulApriRequests(){
+        Spark.get("/", this::echoRequest);
+    }
+    private String echoRequest(Request request, Response response){
+        response.type("application/json");
+        response.header("Access-Control-Allow-Origin", "*");
+        response.status(200);
+        return HttpRequestToJson(request);
+    }
+    private String HttpRequestToJson(Request request) {
+        return "{\n"
+                + "\"attributes\":\"" + request.attributes()  + "\",\n"
+                + "\"body\":\"" + request.body()              + "\",\n"
+                + "\"contentLength\":\"" + request.contentLength()  + "\",\n"
+                + "\"contentType\":\"" + request.contentType() + "\",\n"
+                + "\"cookies\":\"" + request.cookies()  + "\",\n"
+                + "\"headers\":\"" + request.headers()  + "\",\n"
+                + "\"host\":\"" + request.host()  + "\",\n"
+                + "\"ip\":\"" + request.ip()  + "\",\n"
+                + "\"params\":\"" + request.params()  + "\",\n"
+                + "\"pathInfo\":\"" + request.pathInfo()  + "\",\n"
+                + "\"serverPort\":\"" + request.port()  + "\",\n"
+                + "\"protocol\":\"" + request.protocol()  + "\",\n"
+                + "\"queryParams\":\"" + request.queryParams()  + "\",\n"
+                + "\"requestMethod\":\"" + request.requestMethod()  + "\",\n"
+                + "\"scheme\":\"" + request.scheme()  + "\",\n"
+                + "\"servletPath\":\"" + request.servletPath()  + "\",\n"
+                + "\"sessions\":\"" + request.session()  + "\",\n"
+                + "\"uri()\":\"" + request.uri()  + "\",\n"
+                + "\"url()\":\"" + request.url()  + "\",\n"
+                + "\"userAgent\":\"" + request.userAgent()  + "\",\n"
+                + "}";
+
+    }
+    public static void main(String[] programArgs){
+        RestfulServer restfulServer = new RestfulServer();
+
+    }
+}
